17fe8d8cab3e52e4b0345b35c1f24d064a4f4453
patrickmumot123@gmail.com <patrickmumot123@gmail.com>
2017-05-19 21:07:16 +0000

    weekday strings

--- sql_connect.js
+++ sql_connect.js
@@ -96,10 +96,20 @@ function checkSchedule(msg){
 }
 function querySchedule(groupId)
 {
+    var d = new Date();
+    var weekday = new Array(7);
+    weekday[0] =  "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+    var n = weekday[d.getDay()];
     var request1 = new Request(
-            "SELECT Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday FROM Schedule WHERE Id ='"+groupId[2]+"'",
+            "SELECT "+n+" FROM Schedule WHERE Id ='"+groupId[2]+"'",
             function(err1, rowCount1, rows1) {           
-                console.log(rowCount1 + '  =-== returned for group :'+groupId);
+                console.log(rowCount1 + '  =-== returned for group :'+groupId[2]);
             }
         );
         //console.log("='"+groupId[2]+"'");
